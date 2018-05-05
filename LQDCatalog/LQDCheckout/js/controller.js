091cf91740ff1d47e15a89f6082c23041db194cb
Mysterymman <zej777@gmail.com>
2018-05-05 22:08:42 +0800

    1.0.5
    Implemented:
    -Catalog Design
    -Dropdown menu for Nicotine selection
    -Add to Cart Button for Catalog
    -Checkout Design
    
    To be implemented:
    -communication between pages
    -handler for add to cart button and nicotine selection button
    -checkout page functionality

---
+++ LQDCatalog/LQDCheckout/js/controller.js
@@ -0,0 +1,30 @@
+/**
+ * This will be used to control the entire application. 
+ * This is where you will pass feedback from view to the model and vice versa.
+ * This is also where you initialize the Single Page application.
+ */
+
+/**
+ * The module that initializes the SPA.
+ */
+var controller = (function(){
+    function init(){
+        initializeView();
+        initializeNavBarButtons();
+    }return {
+        init: init
+    }
+})()
+
+function initializeNavBarButtons(){
+    $("#aboutNav").click(function(){ 
+        moveToAbout();
+    });
+    $("#productsNav").click(function(){ 
+        moveToProducts();
+    });
+}
+
+function sendProductsToView(){
+    viewProducts(retrieveProducts());
+}
---
+++ LQDCatalog/LQDCheckout/js/view.js
@@ -0,0 +1,26 @@
+﻿var offset;
+var navbar;
+
+/**
+ * When the user scrolls the page past the navbars offset, the navbar will
+ * stick to the top of the screen.
+ */
+window.onscroll = function () { 
+    if ($(document).scrollTop() >= offset.top) {
+        navbar.addClass("sticky")
+    } else {
+        navbar.removeClass("sticky");
+    } 
+};
+
+function initializeView(){
+    navbar = $("#navbar");
+    offset = navbar.offset(); //Gets the current position of the navbar. 
+}
+
+/**
+ * Contains all the interaction between the user and the application.
+ */
+background-color: rgba(51,51,51, 1);
+overflow: hidden;
+position: relative;
